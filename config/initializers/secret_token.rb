c0341b33fba82447d2ddfc840e455b777cf2e95e
Leonardo Mateo <leonardomateo@gmail.com>
2011-10-18 07:45:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musix::Application.config.secret_token = 'f839201eb7005a2e916ab14ef581deb7b267ce10af7a712e2aabebddb7961bba6022e882ccf21e72e36da074ba6a9c90552fd6630975ea2cd1c2d1f22fba1f57'
