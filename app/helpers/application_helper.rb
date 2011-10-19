f1b9fc900da57e3802eafee3879093471de2c978
Leonardo Mateo <leonardomateo@gmail.com>
2011-10-19 11:58:34 -0300

    Adds new, index and flash shared views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def format_messages(messages, title = nil)
+    html = title.nil? ? "" : "<h4>#{title}</h4>"
+    if messages.is_a? Array
+      html << "<ul class='errors'>"
+      messages.each do |msg|
+        html << "<li>#{msg}</li>"
+      end
+      html << "</ul>"
+    else
+      html << messages
+    end
+
+    html
+  end
 end
